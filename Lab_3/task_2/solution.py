2b5fb6250f865f37100f39df3df5fccecc56ccb1
Ilya Antipiev <funnypig1606@gmail.com>
2019-04-04 00:46:21 +0300

    Lab 3. 1st and 2nd tasks

---
+++ Lab_3/task_2/solution.py
@@ -0,0 +1,35 @@
+from scipy.integrate import odeint
+from scipy.optimize import minimize
+import pylab as pl
+import matplotlib.pyplot as plt
+import numpy as np
+
+g = lambda x, y: y**2+3*y+2
+
+plt.figure(figsize=(6,10))
+plt.tight_layout()
+
+
+# Task 2.1
+
+plt.subplot(2,1,1)
+
+x, y = np.meshgrid(np.arange(-4,5),np.arange(-4,5))
+
+plt.quiver(x, y)
+plt.title("Vector field")
+
+# Task 2.2
+
+# g(y0) = 0
+y0 = float(minimize(lambda y: abs(g(0, y)), 0).x)
+
+x = np.arange(-10, 10)
+y = odeint(g, y0, x)
+
+plt.subplot(2,1,2)
+
+plt.plot(x, y)
+plt.title("Integral curve")
+
+plt.show()
